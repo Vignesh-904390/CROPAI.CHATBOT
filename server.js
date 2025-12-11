df8995dfbe888d4e0bd9f6ea71506e4451215336
vignesh904390 <vigneshsrinivasan904390@gmail.com>
2025-12-11 13:23:38 +0530

    commit

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 // server.js
 import express from "express";
 import cors from "cors";
-
+import dotenv from "dotenv";
+import OpenAI from "openai";
 import path from "path";
 import { fileURLToPath } from "url";
 
+dotenv.config();
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -16,7 +18,9 @@ app.use(express.json());
 // Serve frontend
 app.use(express.static(path.join(__dirname, "public")));
 
-
+const client = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
 
 // FIXED CHAT ENDPOINT (WORKS 100%)
 app.post("/chat", async (req, res) => {
